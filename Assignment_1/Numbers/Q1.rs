ced9c6181ec85c3c0ca4f1e4bd6128f1df5595b6
Aalok Tanwar <aalok.singh2020@vitbhopal.ac.in>
2022-07-13 10:31:50 +0530

    Create Q1.rs

---
+++ Assignment_1/Numbers/Q1.rs
@@ -0,0 +1,12 @@
+
+// Remove something to make it work
+fn main() {
+    let x: i32 = 5;
+    let mut y = 5; // removed data type of y
+
+    y = x;
+    
+    let z = 10; // Type of z ? 
+
+    println!("Success!");
+}
