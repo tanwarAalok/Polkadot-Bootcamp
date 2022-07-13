42d8b2f4309ab1aac2e2c70d55569277a5d1c5de
Aalok Tanwar <aalok.singh2020@vitbhopal.ac.in>
2022-07-13 10:38:46 +0530

    Create Q1.rs

---
+++ Assignment_1/Char, bool and Unit/Q1.rs
@@ -0,0 +1,12 @@
+
+// Make it work
+use std::mem::size_of_val;
+fn main() {
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1),4); // corrected size 
+
+    let c2 = '中';
+    assert_eq!(size_of_val(&c2),4); // corrected size 
+
+    println!("Success!");
+} 
