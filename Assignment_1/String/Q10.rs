6a571a970539c5fe4897f58c22c287cddc0bd175
Aalok Tanwar <aalok.singh2020@vitbhopal.ac.in>
2022-07-14 11:10:56 +0530

    Create Q10.rs

---
+++ Assignment_1/String/Q10.rs
@@ -0,0 +1,20 @@
+
+/* Fill in the blank and fix the errors */
+fn main() {
+    let raw_str = r"Escapes don't work here: ? ℝ";  // not sure if this correct way
+    assert_eq!(raw_str, "Escapes don't work here: ? ℝ");
+
+    // If you need quotes in a raw string, add a pair of #s
+    let quotes = r#"And then I said: "There is no escape!""#;
+    println!("{}", quotes);
+
+    // If you need "# in your string, just use more #s in the delimiter.
+    // You can use up to 65535 #s.
+    let  delimiter = r###"A string with "# in it. And even "##!"###;
+    println!("{}", delimiter);
+
+    let long_delimiter = "Hello, \"##\"";
+    assert_eq!(long_delimiter, "Hello, \"##\"");
+
+    println!("Success!");
+}
