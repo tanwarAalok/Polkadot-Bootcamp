e253bbfcc744fc7e7526a2f8e48ed442f04a1d07
Aalok Tanwar <aalok.singh2020@vitbhopal.ac.in>
2022-07-14 15:23:33 +0530

    Create Q5.rs

---
+++ Assignment_1/Struct/Q5.rs
@@ -0,0 +1,16 @@
+
+// Fill the blank
+struct Person {
+    name: String,
+    age: u8,
+}
+fn main() {
+    println!("Success!");
+} 
+
+fn build_person(name: String, age: u8) -> Person {
+    Person {
+        age,
+        name
+    }
+}
