a09d22b1732e6d554eb5d6638cbc3d68a5600b6d
Aalok Tanwar <aalok.singh2020@vitbhopal.ac.in>
2022-07-21 21:51:34 +0530

    Create Q1.rs

---
+++ Assignment_2/Const Generic/Q1.rs
@@ -0,0 +1,19 @@
+struct Array<T, const N: usize> {
+    data : [T; N]
+}
+
+fn main() {
+    let arrays = [
+        Array{
+            data: [1, 2, 3],
+        },
+        Array {
+            data: [1, 2, 3],
+        },
+        Array {
+            data: [1, 2,4]
+        }
+    ];
+
+    println!("Success!");
+}
