bb0e51a570a0eb28ad91cd799b3892d34f033659
Aalok Tanwar <aalok.singh2020@vitbhopal.ac.in>
2022-07-26 10:45:09 +0530

    Create Q2.rs

---
+++ Assignment_2/Vectors/Q2.rs
@@ -0,0 +1,14 @@
+
+// FILL in the blank
+fn main() {
+    let mut v1 = Vec::from([1, 2, 4]);
+    v1.pop();
+    v1.push(3);
+    
+    let mut v2 = Vec::new();
+    v2.extend([1,2,3]);
+
+    assert_eq!(v1, v2);
+
+    println!("Success!")
+}
