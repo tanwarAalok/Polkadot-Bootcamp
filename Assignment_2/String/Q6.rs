7212d97d7e8caff6b18d1531011b563e3d6c8c35
Aalok Tanwar <aalok.singh2020@vitbhopal.ac.in>
2022-07-26 10:38:08 +0530

    Create Q6.rs

---
+++ Assignment_2/String/Q6.rs
@@ -0,0 +1,18 @@
+
+// modify the code below to print out: 
+// 25
+// 25
+// 25
+// Here, there’s no need to allocate more memory inside the loop.
+fn main() {
+    let mut s = String::with_capacity(25);
+
+    println!("{}", s.capacity());
+
+    for _ in 0..2 {
+        s.push_str("hello");
+        println!("{}", s.capacity());
+    }
+
+    println!("Success!")
+}
